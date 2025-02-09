7d603e6ba6e0fc82c6be763231a6c71ac2e480a4
Dizza Harkhani <dharkhani2375@conestogac.on.ca>
2025-02-09 18:08:24 -0500

    Triangle App

---
+++ TriangleTest/Class1.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using NUnitDemonstration;
+
+namespace TriangleTest
+{
+    public class TriangleTest
+    {
+        [TestFixture]
+        public class Class1
+        {
+            [Test]
+            public void ValidTriangle_Input60and60_OutputValidTriangle()
+            {
+                //Arrange
+                int firstAngle = 60;
+                int secondAngle = 60;
+                int thirdAngle = 60;
+
+                string expected = "The triangle is valid";
+
+                //Act
+                string actual = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+
+                //Assert
+                Assert.That(expected, Is.EqualTo(actual));
+            }
+
+        }
+    }
+}
